65ecdd6da9d183d5601f023199d46b2305bb7cb2
Rishabh Rawat <rishabh@remoteward.com>
2025-12-18 06:36:47 +0530

    Topic: Added location map

--- frontend/src/components/LocationMap.jsx
+++ frontend/src/components/LocationMap.jsx
@@ -1,34 +1,38 @@
-import React, { useMemo } from "react";
+import React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 
 // Fix default marker icons
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
-  iconRetinaUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
-  iconUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
-  shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
+  iconRetinaUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
+  iconUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
+  shadowUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
 });
 
 function LocationMap({ latitude, longitude, location, darkMode }) {
-  // Memoize the map component to prevent unnecessary re-renders
-  const mapComponent = useMemo(() => {
-    // Validate coordinates
-    if (!latitude || !longitude || isNaN(latitude) || isNaN(longitude)) {
-      return null;
-    }
+  if (!latitude || !longitude || isNaN(latitude) || isNaN(longitude)) {
+    return null;
+  }
 
-    const position = [latitude, longitude];
+  const position = [latitude, longitude];
 
-    return (
+  return (
+    <div style={{ height: "100%", width: "100%" }}>
       <MapContainer
         center={position}
         zoom={13}
         style={{ height: "100%", width: "100%" }}
         scrollWheelZoom={false}
         zoomControl={true}
+        key={`${latitude}-${longitude}`}
       >
         <TileLayer
+          crossOrigin={true} // FIX: Resolves the CORP/CORS blocking issue
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
           url={
             darkMode
@@ -38,20 +42,20 @@ function LocationMap({ latitude, longitude, location, darkMode }) {
         />
         <Marker position={position}>
           <Popup>
-            {location && (
-              <>
-                <strong>{location}</strong>
-                <br />
-              </>
-            )}
-            {latitude.toFixed(4)}, {longitude.toFixed(4)}
+            <div>
+              {location && (
+                <>
+                  <strong>{location}</strong>
+                  <br />
+                </>
+              )}
+              {latitude.toFixed(4)}, {longitude.toFixed(4)}
+            </div>
           </Popup>
         </Marker>
       </MapContainer>
-    );
-  }, [latitude, longitude, location, darkMode]);
-
-  return mapComponent;
+    </div>
+  );
 }
 
 export default LocationMap;
